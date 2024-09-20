c124d6dfe4c4c71c34cf9d1b82f1c03555c73ee4
adwaith0071 <adwaithb058@gmail.com>
2024-09-20 15:46:26 +0530

    'msg'

--- src/services/allApi.js
+++ src/services/allApi.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const base_url="http://localhost:3000"
+const base_url="https://mpserver-a7ms.onrender.com"
 
 export const addVideoApi=async(data)=>{
     return await axios.post(`${base_url}/videos`,data)
